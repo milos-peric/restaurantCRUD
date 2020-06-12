1b9275e255038975be4b99d7184c0dddbb31c3ff
milos-peric <66783772+milos-peric@users.noreply.github.com>
2020-06-12 08:41:46 +0200

    Add files via upload
    Test commit

---
+++ DAN_XXX_MilosPeric/DAN_XXX_MilosPeric/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DAN_XXX_MilosPeric
+{
+    class Program
+    {
+        public delegate int MyDelegateRandomGenerator();
+        public delegate void MyDelegateCaseHandler(int num);
+        static void Main(string[] args)
+        {
+
+            Console.WriteLine("\nPritisnite ili drzite spejs za generisanje random broja od 1 do 150");
+            ConsoleKeyInfo cki;
+            Utility util = new Utility();
+            MyDelegateRandomGenerator myDelRandGen = util.RandomGenerator;
+            MyDelegateCaseHandler myDelCaseHandl = util.RandomCase;
+            int broj = 0;
+            do
+            {
+                cki = Console.ReadKey(true);
+                if (cki.Key == ConsoleKey.Spacebar)
+                {
+                    broj = myDelRandGen();
+                    Console.WriteLine(broj);
+                }
+                if (cki.Key == ConsoleKey.Enter)
+                {
+                    myDelCaseHandl(broj);
+                }
+            } while (cki.Key != ConsoleKey.X);
+            Console.ReadKey();
+        }
+    }
+}
---
+++ DAN_XXX_MilosPeric/DAN_XXX_MilosPeric/Utility.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DAN_XXX_MilosPeric
+{
+    class Utility
+    {
+        Random random = new Random();
+        int broj = 0;
+
+        public int RandomGenerator()
+        {
+            broj = random.Next(1, 151);
+            return broj;
+        }
+
+        public void RandomCase(int number)
+        {
+            Console.WriteLine("Pritisnut Enter. Broj {0} je snimljen.", number);
+            if (number < 75)
+            {
+                Console.WriteLine($"Broj < 75 => {2}^{number} = {(long)Math.Pow(2, number):N0}");
+            }
+            else if (number > 75)
+            {
+                Console.WriteLine("Broj > 75 => {0} * {0} = {1:N0}", number, number * number);
+            }
+            else if (number == 75)
+            {
+                Console.WriteLine("Broj = 75 => Generisan novi broj {0}", RandomGenerator());
+            }
+        }
+    }
+}
